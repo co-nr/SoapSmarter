90a48f12e270bf81225f6402d7cdde07185cf2fc
Connor Gill <connor@cgill.net>
2025-11-05 13:32:03 +0700

    Initial Commit

---
+++ SoapSmarter/SoapSmarterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoapSmarterApp.swift
+//  SoapSmarter
+//
+//  Created by Connor on 11/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SoapSmarterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
